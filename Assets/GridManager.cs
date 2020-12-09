4db61d85ac868040296ddb08df84f9bc2e69c7b1
gamesketches <sam.von.ehren@gmail.com>
2020-12-08 20:59:19 -0500

    Added starting Grid Map, made color blending functions more obvious

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -10,9 +10,16 @@ public class GridManager : MonoBehaviour
     SpriteRenderer[] grid;
     public static Color currentColor;
     static GridManager instance;
+    Color[] startingGridMap;
     // Start is called before the first frame update
     void Start()
     {
+
+        // Modify this to change starting grid
+        startingGridMap = new Color[] {Color.red, Color.white, Color.red,
+                                       Color.white, Color.red, Color.white,
+                                        Color.red, Color.white, Color.red};
+
         instance = this;
         grid = new SpriteRenderer[gridSize * gridSize];
         currentColor = Color.red;
@@ -20,11 +27,17 @@ public class GridManager : MonoBehaviour
         {
             grid[i] = Instantiate<GameObject>(cellPrefab).GetComponent<SpriteRenderer>();
             grid[i].transform.position = new Vector3((i % gridSize) * cellSize, (i / gridSize) * cellSize, 0);
+            // Colors in via grid map if gridmap is accurate
+            if(gridSize * gridSize == startingGridMap.Length)
+            {
+                grid[i].color = startingGridMap[i];
+            }
             /*if(i % 2 == 1)
             {
                 grid[i].color = Color.LerpUnclamped(Color.white, Color.red, 0.5f);
             }*/
-            if(i == 0 || i == 2 || i == 6 || i == 8)
+            // Color in corners
+            else if ((i % gridSize == 0 || i % gridSize == gridSize - 1) && (i / gridSize == 0 || i / gridSize == gridSize - 1))
             {
                 grid[i].color = Color.LerpUnclamped(Color.white, Color.red, 0.5f);
             }
@@ -50,6 +63,7 @@ public class GridManager : MonoBehaviour
     public static void HandleClick(CellBehavior clickedCell)
     {
         int clickedCellID = -1;
+        // Find the cell
         for(int i = 0; i < instance.grid.Length; i++)
         {
             if(instance.grid[i].GetComponent<CellBehavior>() == clickedCell)
@@ -58,32 +72,47 @@ public class GridManager : MonoBehaviour
                 break;
             }
         }
-        //instance.grid[clickedCellID].color = currentColor;
-        instance.grid[clickedCellID].color = BlendColors(currentColor, instance.grid[clickedCellID].color);
+        instance.CalcAndFillSquare(clickedCellID);
         int horizontalOffset = clickedCellID % instance.gridSize;
+        // Fill to the left if a cell exists to the left
         if(horizontalOffset > 0)
         {
-            //instance.grid[clickedCellID - 1].color = currentColor;
-            instance.grid[clickedCellID - 1].color = BlendColors(currentColor, instance.grid[clickedCellID - 1].color);
-
+            instance.CalcAndFillSquare(clickedCellID - 1);
         }
+        // Fill to the right if a cell exists to the left
         if (horizontalOffset < instance.gridSize - 1)
         {
-            //instance.grid[clickedCellID + 1].color = currentColor;
-            instance.grid[clickedCellID + 1].color = BlendColors(currentColor, instance.grid[clickedCellID + 1].color);
-
+            instance.CalcAndFillSquare(clickedCellID + 1);
         }
         int verticalOffset = clickedCellID / instance.gridSize;
+        // Fill above if a cell exists above
         if (verticalOffset > 0)
         {
-            //instance.grid[clickedCellID - instance.gridSize].color = currentColor;
-            instance.grid[clickedCellID - instance.gridSize].color = BlendColors(currentColor, instance.grid[clickedCellID - instance.gridSize].color);
+            instance.CalcAndFillSquare(clickedCellID - instance.gridSize);
         }
+        // Fill below if a cell below exists
         if (verticalOffset < instance.gridSize - 1)
         {
-            //instance.grid[clickedCellID + instance.gridSize].color = currentColor;
-            instance.grid[clickedCellID + instance.gridSize].color = BlendColors(currentColor, instance.grid[clickedCellID + instance.gridSize].color);
+            instance.CalcAndFillSquare(clickedCellID + instance.gridSize);
         }
+    }
+
+    void CalcAndFillSquare(int targetCell)
+    {
+        Color targetColor = BlendColors(currentColor, instance.grid[targetCell].color);
+        StartCoroutine(FillColors(targetCell, targetColor));
+
+    }
+
+    public static IEnumerator FillColors(int cell, Color newColor)
+    {
+        Color startColor = instance.grid[cell].color;
+        for(float t = 0; t < 0.4f; t+= Time.deltaTime)
+        {
+            instance.grid[cell].color = Color.Lerp(startColor, newColor, t / 0.4f);
+            yield return null;
+        }
+        instance.grid[cell].color = newColor;
     }
 
     public void ChangeColor(string colorHex)
