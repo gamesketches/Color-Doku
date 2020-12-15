f2f53a1d7662a6967bea72c105ed452a4c5c16ba
gamesketches <sam.von.ehren@gmail.com>
2020-12-14 21:36:07 -0500

    got more advanced blending in place

---
+++ Assets/BlendingCellBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendingCellBehavior : MonoBehaviour
+{
+    public ColoringCellBehavior[] linkedCells;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        foreach(ColoringCellBehavior cell in linkedCells)
+        {
+            cell.colorChangeTrigger += BlendColors;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //BlendColors(); 
+    }
+
+    void BlendColors()
+    {
+        Color startColor = spriteRenderer.color;
+        foreach(ColoringCellBehavior cell in linkedCells)
+        {
+            startColor = Color.Lerp(startColor, cell.blendingColor, 0.5f);
+        }
+        if(spriteRenderer.color != startColor) {
+            StartCoroutine(LerpToColor(startColor));
+        }
+    }
+
+    IEnumerator LerpToColor(Color newColor) {
+        Color startColor = spriteRenderer.color;
+        for(float t = 0; t < 1; t += Time.deltaTime)
+        {
+            spriteRenderer.color = Color.Lerp(startColor, newColor, t / 1);
+            yield return null;
+        }
+        spriteRenderer.color = newColor;
+    }
+}
---
+++ Assets/ColoringCellBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColoringCellColor {Red, Blue, Yellow, White}
+public class ColoringCellBehavior : MonoBehaviour
+{
+    public Color blendingColor;
+    ColoringCellColor cellColor = ColoringCellColor.White;
+    SpriteRenderer spriteRenderer;
+    public delegate void OnColorChange();
+    public OnColorChange colorChangeTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        blendingColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        UpdateCellColor();
+    }
+
+    void UpdateCellColor()
+    {
+        switch(cellColor)
+        {
+            case ColoringCellColor.White:
+                cellColor = ColoringCellColor.Blue;
+                blendingColor = Color.blue;
+                break;
+            case ColoringCellColor.Blue:
+                cellColor = ColoringCellColor.Red;
+                blendingColor = Color.red;
+                break;
+            case ColoringCellColor.Red:
+                cellColor = ColoringCellColor.Yellow;
+                blendingColor = Color.yellow;
+                break;
+            case ColoringCellColor.Yellow:
+                cellColor = ColoringCellColor.White;
+                blendingColor = Color.white;
+                break;
+        }
+        spriteRenderer.color = blendingColor;
+        colorChangeTrigger();
+    }
+
+}
